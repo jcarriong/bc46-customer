589c297d09fb6630b4d4b528d9d40e5a005ad5fa
jcarriong <finales.15@gmail.com>
2023-10-06 20:55:37 -0500

    chore: adding eureka client and config client

--- src/main/java/com/nttdata/bc46customer/Bc46CustomerApplication.java
+++ src/main/java/com/nttdata/bc46customer/Bc46CustomerApplication.java
@@ -2,11 +2,13 @@ package com.nttdata.bc46customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Ntt Data - Top Employer 2023.
  * Todos los derechos Reservados.
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class Bc46CustomerApplication {
 
